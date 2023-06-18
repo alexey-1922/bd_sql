b9fafd4288def96fd4aa3370c5c526fdd088f148
Alexey Alyabiev <alexalabev@yandex.ru>
2023-06-18 23:01:05 +0300

    music_table

--- postgres.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(database="netology_db", user="postgres", password="Astrah-20047")
-with conn.cursor() as cur:
-  cur.execute("CREATE TABLE test(id SERIAL PRIMARY KEY);")
-  conn.commit()
-conn.close()
