191afbdf7a77e8dacbac3cd291f11ef825520cd0
Alexey Alyabiev <alexalabev@yandex.ru>
2023-07-02 21:59:03 +0300

    db

--- Script.sql
+++ Script.sql
@@ -1,46 +1,46 @@
 CREATE TABLE IF NOT EXISTS Artist (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(80) NOT NULL
+	artist_id SERIAL PRIMARY KEY,
+	artistname VARCHAR(80) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Genre (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(80) NOT NULL
+	genre_id SERIAL PRIMARY KEY,
+	genrename VARCHAR(80) NOT NULL
 );
 
 create table if not exists Artist_genre (
-	artist_id integer references Artist(id),
-	genre_id integer references Genre(id),
+	artist_id integer references Artist(artist_id),
+	genre_id integer references Genre(genre_id),
 	CONSTRAINT pk PRIMARY KEY (artist_id, genre_id)
 );
 
 create table if not exists Album (
-	id serial primary key,
-	name VARCHAR(80) NOT null,
+	album_id serial primary key,
+	albumname VARCHAR(80) NOT null,
 	release integer not null
 );
 
 create table if not exists Artist_album (
-	artist_id integer references Artist(id),
-	album_id integer references Album(id),
+	artist_id integer references Artist(artist_id),
+	album_id integer references Album(album_id),
 	constraint pk2 primary key (artist_id, album_id)
 );
 
 create table if not exists Track (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(80) NOT NULL,
+	track_id SERIAL PRIMARY KEY,
+	trackname VARCHAR(80) NOT NULL,
 	duration integer NOT NULL,
-	album_id integer references Album(id)	
+	album_id integer references Album(album_id)	
 );
 
 create table if not exists Collection (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(80) NOT NULL,
+	collection_id SERIAL PRIMARY KEY,
+	collectionname VARCHAR(80) NOT NULL,
 	release integer NOT null
 );
 
 create table if not exists Collection_track (
-	track_id integer references Track(id),
-	collection_id integer references Collection(id),
+	track_id integer references Track(track_id),
+	collection_id integer references Collection(collection_id),
 	constraint pk3 primary key (track_id, collection_id)
 );
--- music_table.sql
+++ music_table.sql
@@ -1,53 +1,57 @@
-#Задание 2
-#Название и продолжительность самого длительного трека.
-SELECT name, duration FROM track
+/*Задание 2
+Название и продолжительность самого длительного трека.*/
+SELECT trackname, duration FROM track
 where duration = (select max(duration) from track);
 
-#Название треков, продолжительность которых не менее 3,5 минут.
-SELECT name, duration FROM track
-where duration >= 3.5;
+--Название треков, продолжительность которых не менее 3,5 минут.
+SELECT trackname, duration FROM track
+where duration >= 210;
 
-#Названия сборников, вышедших в период с 2018 по 2020 год включительно. (для корректности с 1982 по 2007 сделал).
-SELECT name, release FROM collection
+--Названия сборников, вышедших в период с 2018 по 2020 год включительно. (для корректности с 1982 по 2007 сделал).
+SELECT collectionname, release FROM collection
 where  release between 1982 and 2007;
 
-#Исполнители, чьё имя состоит из одного слова.
-SELECT name FROM artist
-WHERE name NOT LIKE '% %';
+--Исполнители, чьё имя состоит из одного слова.
+SELECT artistname FROM artist
+WHERE artistname NOT LIKE '% %';
 
-#Название треков, которые содержат слово «мой» или «my».
-SELECT name FROM track
-WHERE name LIKE '%My%' or name like '%мой%'
+--Название треков, которые содержат слово «мой» или «my».
+SELECT trackname FROM track
+WHERE trackname ILIKE 'my %' or trackname ilike '% my %' or trackname ilike '% my' or trackname ilike 'my'
 
-#Задание 3
-#Количество исполнителей в каждом жанре.
+/*Задание 3
+Количество исполнителей в каждом жанре.*/
 SELECT genrename, COUNT(artistname) FROM genre g
-JOIN artist_genre ag ON g.id = ag.genre_id
-JOIN artist a ON ag.artist_id = a.id
+JOIN artist_genre ag ON g.genre_id = ag.genre_id
+JOIN artist a ON ag.artist_id = a.artist_id
 GROUP BY genrename;
 
-#Количество треков, вошедших в альбомы 2019–2020 годов (для корректности с 1982 по 2007 сделал).
+--Количество треков, вошедших в альбомы 2019–2020 годов (для корректности с 1982 по 2007 сделал).
 SELECT albumname, COUNT(trackname) FROM album a
-JOIN track t ON a.id = t.id
+JOIN track t ON a.album_id = t.track_id
 WHERE release BETWEEN 1982 AND 2007
 GROUP BY albumname;
 
-#Средняя продолжительность треков по каждому альбому.
+SELECT COUNT(trackname) FROM track t 
+join album a ON t.album_id = a.album_id 
+WHERE release BETWEEN 1982 and 2007; 
+
+--Cредняя продолжительность треков по каждому альбому.
 SELECT albumname, AVG(duration) FROM album a
-JOIN track t ON a.id = t.id
+JOIN track t ON a.album_id = t.album_id
 GROUP BY albumname;
 
-#Все исполнители, которые не выпустили альбомы в 2020 году. (для корректности в 2011 сделал).
+--Все исполнители, которые не выпустили альбомы в 2020 году. (для корректности в 2011 сделал).
 SELECT artistname, release FROM artist ar
-JOIN artist_album aa ON ar.id = aa.album_id
-JOIN album al ON aa.album_id = al.id
+JOIN artist_album aa ON ar.artist_id = aa.album_id
+JOIN album al ON aa.album_id = al.album_id
 WHERE release != 2011
 
-#Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
+--Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 SELECT c.collectionname FROM collection c
-JOIN collection_track ct ON c.id = ct.collection_id
-JOIN track t ON ct.track_id = t.id
-JOIN album al ON t.id = al.id
-JOIN artist_album aa ON al.id = aa.album_id
-JOIN artist ar ON aa.artist_id = ar.id
+JOIN collection_track ct ON c.collection_id = ct.collection_id
+JOIN track t ON ct.track_id = t.track_id
+JOIN album al ON t.track_id = al.album_id
+JOIN artist_album aa ON al.album_id = aa.album_id
+JOIN artist ar ON aa.artist_id = ar.artist_id
 WHERE ar.artistname = 'Beyonce'
